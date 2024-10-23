d7924847b055b9aabb2f42ae5f09d0cab9202755
BojanJankov <150026975+BojanJankov@users.noreply.github.com>
2024-10-23 16:06:40 +0200

    updated navbar with backround color and new hover color, updated some padding for all section containers, added two projcets in projcets section, updated images and some css

--- src/Components/Projcets/Projcets.tsx
+++ src/Components/Projcets/Projcets.tsx
@@ -8,6 +8,26 @@ function Projcets() {
       </div>
       <div className="projects-div-container">
         <a
+          href="https://github.com/BojanJankov/thought-exchange-app"
+          target="_blank"
+          className="card"
+        >
+          <img
+            src="/exchange-app.png"
+            alt="balloon with an emoji face"
+            className="card__img"
+          />
+          <span className="card__footer">
+            <span>Thought exchange app</span>
+            <span>Angular/Express/MongoDB/Firebase</span>
+          </span>
+          <span className="card__action">
+            <svg viewBox="0 0 448 512">
+              <path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z" />
+            </svg>
+          </span>
+        </a>
+        <a
           href="https://github.com/BojanJankov/E-Store-App"
           target="_blank"
           className="card"
@@ -27,7 +47,26 @@ function Projcets() {
             </svg>
           </span>
         </a>
-
+        <a
+          href="https://github.com/BojanJankov/Packing-App"
+          target="_blank"
+          className="card"
+        >
+          <img
+            src="/packing-app.png"
+            alt="balloon with an emoji face"
+            className="card__img"
+          />
+          <span className="card__footer">
+            <span>Packing App</span>
+            <span>React + TypeScript</span>
+          </span>
+          <span className="card__action">
+            <svg viewBox="0 0 448 512">
+              <path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z" />
+            </svg>
+          </span>
+        </a>
         <a
           href="https://github.com/BojanJankov/job-seeker-app"
           target="_blank"
