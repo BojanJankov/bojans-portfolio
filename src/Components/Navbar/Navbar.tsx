091b621aee474c22a9be05300bf8d3abcaca9ae7
BojanJankov <150026975+BojanJankov@users.noreply.github.com>
2024-12-22 21:33:05 +0100

    added hamburger menu in header and make all pages responsive for mobile dimensions

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import "./Navbar.css";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toogleHamburger = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="Navbar">
       <div className="logo-div">
@@ -23,6 +30,30 @@ function Navbar() {
           <a href="/contact">CONTACT</a>
         </li>
       </ul>
+      <div className="hamburger-menu">
+        <i className="fa-solid fa-bars" onClick={toogleHamburger}></i>
+        {isOpen && (
+          <div className="navigation">
+            <ul>
+              <li>
+                <a href="/">HOME</a>
+              </li>
+              <li>
+                <a href="/about">ABOUT ME</a>
+              </li>
+              <li>
+                <a href="/projects">PROJCETS</a>
+              </li>
+              <li>
+                <a href="/skills">SKILLS</a>
+              </li>
+              <li>
+                <a href="/contact">CONTACT</a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
     </nav>
   );
 }
